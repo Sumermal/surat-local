2f0a27607dc2c6eb5c261d194598ee298e38eed9
Lohar Sumermal <sumermallohar@gmail.com>
2026-01-17 19:33:17 +0530

    Fix Google site verification

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://surat-local.vercel.app/sitemap.xml",
+  };
+}
